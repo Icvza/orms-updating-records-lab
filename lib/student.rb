ae5f322b8151d70086b33fa078c4c686ca0efdda
Isaac <devisaacalejandro@gmail.com>
2021-01-24 00:23:08 -0500

    commit tree 1d5c438b4e3df6e2d00a311111f2a56255b709dd

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,78 @@ require_relative "../config/environment.rb"
 class Student
     attr_accessor :name, :grade
     attr_reader :id
-  def initialize (name, grade id = nil)
+  def initialize (id = nil, name, grade)
+    @id = id
     @name = name 
     @grade = grade 
-    @id = id
   end
+
+  def self.create_table
+    sql = <<-DOC
+    CREATE TABLE students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    DOC
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-DOC
+    DROP TABLE students
+    DOC
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-DOC
+      
+      INSERT INTO students (name, grade) VALUES (?, ?)
+
+      DOC
+      DB[:conn].execute(sql, self.name, self.grade,)
+    @id = DB[:conn].execute("SELECT last_insert_rowid()FROM students")[0][0]
+    end
+  end
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(row)
+    new_student = self.new(row[0], row[1], row[2])
+    new_student.id == row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end 
+
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
+
+
+
+ 
+
+
+  
+
+
 
 
 end
